fc6a89d7c5303c10809a49d755363a18b76cb82e
Bishal Prasad <biprasad@microsoft.com>
2016-12-27 14:16:06 +0530

    Update cleanup-docker-disk-space.sh

--- cleanup-docker-disk-space.sh
+++ cleanup-docker-disk-space.sh
@@ -1,2 +1,2 @@
-docker ps --all -aq | xargs docker rm -v --force
+docker ps --filter status=dead --filter status=exited -aq | xargs docker rm -v --force
 docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r docker rmi --force
